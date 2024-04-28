d5bb00c39befb2d51158406e9a01b1ab87f54144
Muhammad Halindra Prakoso <muhammad.halindra.prakoso@gmail.com>
2024-04-28 23:38:37 +0700

    init

---
+++ app/components/conform/Input.tsx
@@ -0,0 +1,34 @@
+import { getInputProps, type FieldMetadata } from "@conform-to/react";
+import { useId, type ComponentProps } from "react";
+import { Label } from "~/components/ui/label";
+import { cn } from "~/lib/utils";
+import { Input } from "../ui/input";
+
+export const InputConform = ({
+	meta,
+	type,
+	label,
+	className,
+	...props
+}: {
+	meta: FieldMetadata<string>;
+	type: Parameters<typeof getInputProps>[1]["type"];
+	label?: string;
+	className?: string;
+} & ComponentProps<typeof Input>) => {
+	const fallbackId = useId();
+	const id = props.id ?? fallbackId;
+
+	return (
+		<div className={cn("w-full", className)}>
+			{label && <Label htmlFor={id}>{label}</Label>}
+			<Input
+				{...getInputProps(meta, { type, ariaAttributes: true })}
+				{...props}
+			/>
+			{meta.errors && (
+				<p className="text-sm text-red-500">{meta.errors.join(", ")}</p>
+			)}
+		</div>
+	);
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,81 @@
-import type { MetaFunction } from "@remix-run/node";
+import { getFormProps, useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, json, redirect, useActionData } from "@remix-run/react";
+import { z } from "zod";
+import { InputConform } from "~/components/conform/Input";
+import { RadioGroupConform } from "~/components/conform/RadioGroup";
+import { Button } from "~/components/ui/button";
+import { Card } from "~/components/ui/card";
+import { zodText } from "~/lib/utils";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "Registrasi Raudhah & Insan Rabbany" },
+		{
+			name: "description",
+			content: "Selamat datang di lembar registrasi sekolah",
+		},
+	];
 };
 
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+const schema = z.object({
+	fullname: zodText,
+	nickname: zodText,
+	gender: z.enum(["MALE", "FEMALE"]),
+});
+
+export default function Page() {
+	const actionData = useActionData<typeof action>();
+
+	const [form, field] = useForm<z.infer<typeof schema>>({
+		id: "registration-form",
+		lastResult: actionData?.result,
+		onValidate: ({ formData }) => parseWithZod(formData, { schema }),
+		defaultValue: {
+			gender: "MALE",
+		},
+	});
+
+	return (
+		<div className="p-10">
+			<Card className="p-10 max-w-xl">
+				<Form method="POST" {...getFormProps(form)} className="space-y-5">
+					<InputConform
+						meta={field.fullname}
+						type="text"
+						label="Nama Lengkap Siswa"
+					/>
+					<InputConform
+						meta={field.nickname}
+						type="text"
+						label="Nama Panggilan Siswa"
+					/>
+					<RadioGroupConform
+						meta={field.gender}
+						items={[
+							{ value: "MALE", label: "Laki-laki" },
+							{ value: "FEMALE", label: "Perempuan" },
+						]}
+					/>
+					<Button type="submit" className="w-full">
+						Kirim
+					</Button>
+				</Form>
+			</Card>
+		</div>
+	);
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	const submission = parseWithZod(await request.formData(), { schema });
+	if (submission.status !== "success")
+		return json(
+			{ result: submission.reply() },
+			{ status: submission.status === "error" ? 400 : 200 },
+		);
+
+	const input = submission.value;
+
+	return redirect(".");
 }
